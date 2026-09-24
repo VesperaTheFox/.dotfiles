33ce6812dde20004cf2f321264d76e3a93a7a3c2
VesperaTheFox <michaelplblum@gmail.com>
2026-09-23 18:34:58 -0700

    fix: replace store symlinks with real config file contents

---
+++ home/dotfiles/hypr/hyprland.lua
@@ -0,0 +1,13 @@
+------------------
+---- HYPRLAND ----
+------------------
+
+require("modules.autostart")
+require("modules.decorations")
+require("modules.input")
+require("modules.keybinds")
+require("modules.layouts")
+require("modules.misc")
+require("modules.monitors")
+require("modules.variables")
+require("modules.windrules")
---
+++ home/dotfiles/hypr/modules/monitors.lua
@@ -0,0 +1,11 @@
+------------------
+---- MONITORS ----
+------------------
+-- SWITCH THIS TO FIT YOUR SCRREEN(s)!!!
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+    output   = "DP1",
+    mode     = "5120x1440@144",
+    position = "auto",
+    scale    = "1",
+})
